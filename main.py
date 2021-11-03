fe0fc8a8a028afe3fb43a4901c87a34cce7ad0ee
Alex Xiao <88898856+Hackerchan1227@users.noreply.github.com>
2021-11-03 15:53:19 +0000

    Got Rid of stuff .

--- main.py
+++ main.py
@@ -1,38 +1,52 @@
-#-----import statements-----
 import turtle as trtl
 import random as rand
- 
-#-----game configuration----
+
+trtl.speed(0)
 wn = trtl.Screen()
-wn.setup(width=997, height=700)
-scolor = "red"
-wn.addshape("HuTaoSmug.gif")
-s = "circle"
-ssize = 2
- 
-#-----initialize turtle-----
-sturt = trtl.Turtle()
-sturt.speed(0)
-sturt.shape(s)
-sturt.color(scolor)
-sturt.shapesize(ssize)
- 
+wn.bgcolor("black")
+#-------------------maze and turtle config variables-----------------
+screen_h = 400
+screen_w = 420
+startx = 0
+starty = 0
+turtle_scale = 1.5
+
+#---------------------------GHOST COMMANDS---------------------------
+#------ red ghost commands
+
+#----- init screen
+
+trtl.setup(width=screen_w, height=screen_h)
+HuTao_image = "HuTao.gif"
+trtl.addshape(HuTao_image)
+
+#----- init ghost
+hutao = trtl.Turtle(shape=HuTao_image)
+hutao.hideturtle()
+hutao.pencolor("red")
+hutao.penup()
+hutao.setheading(90)
+hutao.turtlesize(turtle_scale, turtle_scale)
+hutao.goto(0, 0)
+hutao.speed(2)
+hutao.showturtle()
+
 #-----game functions--------
  
  
 def change_position():
   new_xpos = rand.randint(-200, 200)
   new_ypos = rand.randint(-150, 150)
-  sturt.hideturtle()
-  sturt.goto(new_xpos, new_ypos)
-  sturt.showturtle()
+  hutao.hideturtle()
+  hutao.goto(new_xpos, new_ypos)
+  hutao.showturtle()
  
 def s_clicked(x, y):
   change_position()
  
 #-----events----------------
-sturt.penup()
-sturt.onclick(s_clicked)
+hutao.penup()
+hutao.onclick(s_clicked)
  
  
 wn = trtl.Screen()
